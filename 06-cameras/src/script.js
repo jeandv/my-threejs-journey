086afb4cb4d6acda3551c1aa99dad02e417c8502
jeandv <jeanrdev@gmail.com>
2025-08-25 00:17:39 -0400

    [CHAPTER-01-CLASS-06] cameras - update camera with mouse events, use OrthographicCamera, view and use controls like OrbitControls, FirstPersonControls, FlyControls, PointerLockControls, TrackballControls, TransformControls, DragControls...

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,4 +1,21 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+console.log(OrbitControls)
+
+/**
+ * Cursor
+ */
+
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = - (event.clientY / sizes.height - 0.5);
+ });
 
 /**
  * Base
@@ -18,18 +35,34 @@ const scene = new THREE.Scene()
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.MeshBasicMaterial({ color: 0x7d2181 })
 )
 scene.add(mesh)
 
 // Camera
-//const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(
+//     - 1 * aspectRatio, 
+//     1 * aspectRatio,
+//     1,
+//     - 1,
+//     0.1, 
+//     100
+// );
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+//controls.target.y = 1;
+//controls.update();
+controls.enableDamping = true; // nota: SIEMPRE AL USAR DAMPING SE DEBE HACER UN UPDATE DE CONTROLES DESPUES DE HACER UN TICK, ES DECIR EN CADA FRAME, DENTRO DE LA FUNCION tick()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,10 +78,22 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
+
+    //Update camera
+    // camera.position.x = cursor.x * 10;
+    // camera.position.y = cursor.y * 10;
+
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+    // camera.position.y = cursor.y * 3;
+    // camera.lookAt(mesh.position);
 
     // Render
     renderer.render(scene, camera)
+
+    // Update controls
+    controls.update();
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
