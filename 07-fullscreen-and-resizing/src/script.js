d12f65433fa86e955640b20cf650ba0924f07e15
jeandv <jeanrdev@gmail.com>
2025-08-29 01:47:26 -0400

    [CHAPTER-01-CLASS-07] fullscreen and resizing

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -14,7 +14,9 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x7d2181 })
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('/textures/dross.jpg');
+const material = new THREE.MeshBasicMaterial({ map: texture });
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
