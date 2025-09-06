6367b88373254cdfb7a1729bee67713f1b234e1a
jeandv <jeanrdev@gmail.com>
2025-09-06 15:15:38 -0400

    [CHAPTER-01-CLASS-08] geometries

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -11,8 +11,38 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+
+// const positionArray = new Float32Array([
+//     0, 0, 0,
+//     0, 1, 0,
+//     1, 0, 0
+// ]);
+
+// const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
+
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute('position', positionAttribute);
+
+const geometry = new THREE.BufferGeometry();
+
+const count = 500;
+const total = count * 3 * 3
+
+const positionArray = new Float32Array(total);
+
+for (let i = 0; i < total; i++ ) {
+    positionArray[i] = (Math.random() - 0.5) * 4;
+}
+
+const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
+geometry.setAttribute('position', positionAttribute);
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000, 
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
