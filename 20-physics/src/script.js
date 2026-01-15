17204a67f07b9b9b2a47236bcca09700d13f9d11
jeandv <jeanrdev@gmail.com>
2026-01-15 00:31:20 -0400

    20 - physics

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -20,6 +20,15 @@ debugObject.createSphere = () => {
 
 gui.add(debugObject, 'createSphere')
 
+debugObject.createBox = () => {
+    createBox(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5, {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3
+    })
+}
+gui.add(debugObject, 'createBox')
+
 /**
  * Base
  */
@@ -221,6 +230,39 @@ const createSphere = (radius, position) => {
 }
 
 createSphere(0.5, { x: 0, y: 3, z: 0 })
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+})
+
+const createBox = (width, height, depth, position) => {
+    const mesh = new THREE.Mesh(
+        boxGeometry,
+        boxMaterial
+    )
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+    const body = new CANNON.Body({
+        mass: 1,
+        shape,
+        position: new CANNON.Vec3(0, 3, 0),
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    world.addBody(body)
+
+    objectsToUpdate.push({ mesh, body })
+}
+
+createBox(1, 1, 1, { x: 2, y: 3, z: 0 })
 
 
 
